2c7a71d20ef7fc21f5d2a63372836194d231d8a3
KYKY13378 <57913163+KYKY13378@users.noreply.github.com>
2026-04-17 17:57:06 +0300

    Add files via upload

---
+++ control-work-1-main/app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from models import User, Feedback
+
+app = FastAPI()
+
+user_instance = User(name="Никитос Тришин Дмитриевич52", id=1)
+
+feedbacks = []
+
+@app.get("/")
+def read_root():
+    return FileResponse("index.html")
+
+@app.get("/users")
+def get_user():
+    return user_instance
+
+@app.post("/feedback")
+def create_feedback(feedback: Feedback):
+    feedbacks.append(feedback)
+    return {"message": f"Feedback received. Thank you, {feedback.name}."}
